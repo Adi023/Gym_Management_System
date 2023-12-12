598419afa49b03a355a5095a1e2e0ac162f9122e
Adi023 <adityapandhare050@gmail.com>
2023-12-12 22:38:18 +0530

    Input Validation

--- src/components/member/AddMember.jsx
+++ src/components/member/AddMember.jsx
@@ -31,7 +31,7 @@ export default function AddMember() {
         <input type='text' {...register('member_address')} placeholder={"Enter Address"} required/><br/>
 
         <label>Mobile Number : </label>
-        <input type='number' {...register('member_mob_no')} placeholder={"Enter Mobile Number"} required/><br/>
+        <input type='number' {...register('member_mob_no')} min={10} max={10} placeholder={"Enter Mobile Number"} required/><br/>
 
         <label>Email Id : </label>
         <input type='email' {...register('member_email')} placeholder={"Enter Emai-ID"} required/><br/>
@@ -40,10 +40,10 @@ export default function AddMember() {
         <input type='text' {...register('member_gender')} placeholder={"Enter Gender"} required/><br/>
 
         <label>Date of Birth : </label>
-        <input type='date' {...register('member_DOB')} placeholder={"Enter DOB"} required/><br/>
+        <input type='date' {...register('member_DOB')} min={'1950-01-01'} placeholder={"Enter DOB"} required/><br/>
 
         <label>Date Of Joining : </label>
-        <input type='date' {...register('member_DOJoining')} placeholder={"Enter DOJ"} required/><br/>
+        <input type='date' {...register('member_DOJoining')} min={'2023-01-01'} placeholder={"Enter DOJoining"} required/><br/>
 
         <label>Active Status : </label>
         <input type='text' {...register('member_status')} placeholder={"Enter Satus"} required/><br/>
