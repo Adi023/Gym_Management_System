1bac88fb6f16bab6651991dce333a3297dfdb173
Adi023 <adityapandhare050@gmail.com>
2023-12-20 16:26:05 +0530

    Sticky top NAVBAR

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
+import logo from '../../src/components/images/back.jpg'
 
 export default function Header() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleScroll = () => {
+    setShowNavbar(window.scrollY > 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Navbar bg="black" variant="dark" expand="lg" >
-        <Navbar.Brand href="/"><i className='fas fa-dumbbell' style={{fontSize:'28px',color:' rgb(153, 225, 20)'}}>GMS</i></Navbar.Brand>
+      <Navbar  className={showNavbar ? 'fixed-top' : ''} bg="black" variant="dark" expand="lg" >
+        {/* <Navbar.Brand href="/"><i className='fas fa-dumbbell' style={{ fontSize: '28px', color: ' rgb(153, 225, 20)' }}>GMS</i></Navbar.Brand> */}
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" width="100" height="100" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 navbar-nav-scroll" style={{ '--bs-scroll-height': '100px',height:'100px' }}>
+          <Nav className="me-auto my-2 my-lg-0 navbar-nav-scroll" style={{ '--bs-scroll-height': '100px', height: '100px' }}>
             <Nav.Link href="/" active>
               Home
             </Nav.Link>
@@ -29,7 +45,7 @@ export default function Header() {
           </Nav>
           <Form className="d-flex">
             <FormControl type="search" placeholder="Search" aria-label="Search" />
-            <Button variant="outline-success" type="submit" style={{margin: "5px"}}>
+            <Button variant="outline-success" type="submit" style={{ margin: "5px" }}>
               Search
             </Button>
           </Form>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -112,7 +112,7 @@ export default function Home() {
             2 days ago
           </div>
         </div>
-      </div>
+      </div> 
       <Footer/>
     </>
 
