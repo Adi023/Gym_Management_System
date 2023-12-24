aa2943a92c3c7988637a94f842e4f0c72af24b99
Adi023 <adityapandhare050@gmail.com>
2023-12-24 23:54:54 +0530

    images added in about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Footer from './Footer'
+import conta from '../../src/components/images/about.jpg'
 
 export default function AboutUs() {
   return (
-    <><br/><br/><br/>
+    <div>
+        <div className='card' style={{ border: 'black' }}>
+        <img src={conta} className="d-block w-100 " style={{ height: '450px' }} alt='not available' />
+
+        <div className='card-img-overlay' style={{ marginTop: '250px' }}>
+        <h1 className="text-center"style={{  color:"white"}}>About<span style={{ color: '#00ca4e' }}> GMS</span></h1><br /><br /><br />
+
+        </div>
+
+      </div>
+        <br/><br/><br/>
      <div className='container py-4 ' style={{
     display: 'flex', flexWrap: 'wrap',
     background: '#ffffff',
     boxShadow: ' 20px 20px 60px #d9d9d9,-20px -20px 60px #ffffff'
 }}>
-    <h1 className="text-center"style={{  textAlign: 'center', width: '100%' }}>About<span style={{ color: '#00ca4e' }}> GMS</span></h1><br /><br /><br />
+    {/* <h1 className="text-center"style={{  textAlign: 'center', width: '100%' }}>About<span style={{ color: '#00ca4e' }}> GMS</span></h1><br /><br /><br /> */}
 
     <p>GMS provides a 24/7 Fitness facility to residents of Martinsville and Henry County, as well as surrounding areas to help people reach and maintain their goals. We combine different types of fitness equipment to meet different fitness needs and levels.
         <br /><br />
@@ -21,6 +32,6 @@ export default function AboutUs() {
 
 <br/><br/>
         <Footer/>
-    </>
+    </div>
   )
 }
