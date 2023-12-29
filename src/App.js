09d6cbbb2f4aa4647fc924e1c684bb5a7edccc79
Adi023 <adityapandhare050@gmail.com>
2023-12-29 20:01:06 +0530

    feat : added loading component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AllRoutes from "./components/AllRoutes";
 import Header from "./components/Header";
 import './App.css'
+import Loading from "./components/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 2 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    
-    <div >
-      <Header></Header>
-      <AllRoutes></AllRoutes>
+    <div className="App">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        // Your application content goes here
+        <>
+          <Header />
+          <AllRoutes />
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import gymloader from '../components/images/giphy.gif'
+
+const GymLoader = () => {
+  return (
+    <div className="loading-container text-center">
+      <img src={gymloader} alt="Loading" className="loading-gif img-fluid" />
+    </div>
+  );
+}
+
+export default GymLoader;
