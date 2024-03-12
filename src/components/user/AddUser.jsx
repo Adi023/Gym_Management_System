9993aa245df79c3f7b81544e8c74c8a80dfe072f
Adi023 <adityapandhare050@gmail.com>
2024-03-13 00:17:29 +0530

    refactor : register feilds added

--- src/components/user/AddUser.jsx
+++ src/components/user/AddUser.jsx
@@ -1,84 +1,93 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
+import UserServices from '../../services/UserServices'
 
 export default function AddUser() {
   const {register,handleSubmit, formState: { errors },reset}=useForm();
+
+  const sendData=(d)=>{
+    UserServices.addUser(d);
+    alert("User Added Successfully")
+    reset();
+  }
+
   return (
     <div>AddUser
-      <form>
+       <form onSubmit={handleSubmit(sendData)}>
         <label>User Id</label>
-        <input type="text" placeholder={"userId"} />
+        <input type="text" {...register('userId', { required: 'userId is required' })} placeholder={"userId"} />
          {errors.userId && <p style={{ color: 'red' }}>{errors.userId.message}</p>}
         <br/>
 
         <label>Name</label>
-        <input type="text" placeholder={"name"} />
+        <input type="text" {...register('name', { required: 'name is required' })} placeholder={"name"} />
          {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
         <br/>
 
         <label>Email</label>
-        <input type="text" placeholder={"email"} />
+        <input type="text" {...register('email', { required: 'email is required' })} placeholder={"email"} />
          {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
         <br/>
 
         <label>Password</label>
-        <input type="text" placeholder={"password"} />
+        <input type="text" {...register('password', { required: 'password is required' })} placeholder={"password"} />
          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
         <br/>
 
         {/* <label></label>
-        <input type="text" placeholder={"re-enter password"} />
+        <input type="text" {...register('', { required: ' required' })} placeholder={"re-enter password"} />
          {errors. && <p style={{ color: 'red' }}>{errors..message}</p>}
         <br/> */}
 
         <label>Mobile</label>
-        <input type="text" placeholder={"mobile"} />
+        <input type="text" {...register('mobile', { required: 'mobile number is required' })} placeholder={"mobile"} />
          {errors.mobile && <p style={{ color: 'red' }}>{errors.mobile.message}</p>}
         <br/>
 
         <label>Address</label>
-        <input type="text" placeholder={"address"} />
+        <input type="text" {...register('address', { required: 'address is required' })} placeholder={"address"} />
          {errors.address && <p style={{ color: 'red' }}>{errors.address.message}</p>}
         <br/>
 
         <label>City</label>
-        <input type="text" placeholder={"city_Id"} />
+        <input type="text" {...register('city_Id', { required: 'city_Id is required' })} placeholder={"city_Id"} />
          {errors.city_Id && <p style={{ color: 'red' }}>{errors.city_Id.message}</p>}
         <br/>
 
         <label>Date Of Joining</label>
-        <input type="date" placeholder={"date_of_joining"} />
+        <input type="date" {...register('date_of_joining', { required: 'date_of_joining is required' })} placeholder={"date_of_joining"} />
          {errors.date_of_joining && <p style={{ color: 'red' }}>{errors.date_of_joining.message}</p>}
         <br/>
 
         <label>Date Of Birth</label>
-        <input type="date" placeholder={"dob"} />
+        <input type="date" {...register('dob', { required: 'dob is required' })} placeholder={"dob"} />
          {errors.dob && <p style={{ color: 'red' }}>{errors.dob.message}</p>}
         <br/>
 
         <label>Active</label>
-        <input type="text" placeholder={"active"} />
+        <input type="text" {...register('active', { required: 'active is required' })} placeholder={"active"} />
          {errors.active && <p style={{ color: 'red' }}>{errors.active.message}</p>}
         <br/>
 
         <label>Gender</label>
-        <input type="text" placeholder={"gender"} />
+        <input type="text" {...register('gender', { required: 'gender is required' })} placeholder={"gender"} />
          {errors.gender && <p style={{ color: 'red' }}>{errors.gender.message}</p>}
         <br/>
 
         <label>Blood Group</label>
-        <input type="text" placeholder={"bloodGroup"} />
+        <input type="text" {...register('bloodGroup', { required: 'bloodGroup is required' })} placeholder={"bloodGroup"} />
          {errors.bloodGroup && <p style={{ color: 'red' }}>{errors.bloodGroup.message}</p>}
         <br/>
 
         <label>Role</label>
-        <input type="text" placeholder={"USER"}  defaultValue={10}/>
+        <input type="text" {...register('role_Id', { required: 'role_Id is required' })} placeholder={"USER"}  defaultValue={10}/>
          {errors.role_Id && <p style={{ color: 'red' }}>{errors.role_Id.message}</p>}
         <br/>
 
         <label></label>
-        <input type="button" value={"Register"} placeholder={"Register"} />
+        <input type="submit"  value={"Register"} placeholder={"Register"} />
       </form>
+     
     </div>
   )
 }
