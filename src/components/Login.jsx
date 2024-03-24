2558204883101365522f0f153a6afa65beb0ec53
Adi023 <adityapandhare050@gmail.com>
2024-03-24 21:13:49 +0530

    feat : Login component added

---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Dashboard from './user/Dashboard';
+import UserLogin from './user/UserLogin';
+
+export default function Login() {
+
+    const [role, setRole] = useState(null);
+
+    const handleLogin = (userRole) => {
+      setRole(userRole);
+    };
+
+  return (
+    <div>{role ? (
+        <Dashboard role={role} />
+      ) : (
+        <UserLogin onLogin={handleLogin} />
+      )}</div>
+  )
+}
