403c0a1c1439c529bc3aa2bf090c486491622b48
Adi023 <adityapandhare050@gmail.com>
2024-03-27 23:30:43 +0530

    refactor : added useselector from redux

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import AllRoutes from "./components/AllRoutes";
 import Header from "./components/Header";
 import './App.css'
 import Loading from "./components/Loading";
+import { useSelector } from "react-redux";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
  
   useEffect(() => {
     // Simulate loading for 1 seconds
+    // handleLogin();
     setTimeout(() => {
       setIsLoading(false);
     }, 1000);
   }, []);
 
+  // const [role, setRole] = useState(null);
+
+  // const handleLogin = (userRole) => {
+  //   setRole(userRole);
+  //   console.log(userRole+"App.js");
+  // };
+  // const [role, setRole] = useState(null);
+
+  const role = useSelector(state => state.role);
+
   return (
+
+    
     <div className="App">
       {isLoading ? (
         <Loading />
       ) : (
        <>
-          <Header />
+          <Header role={role}/>
           <AllRoutes />
         </>
       )}
-
-
     </div>
   );
 }
