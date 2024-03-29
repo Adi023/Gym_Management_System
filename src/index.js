a47aec669d4231db3f4c39bad7b1aa0ebc86f6e9
Adi023 <adityapandhare050@gmail.com>
2024-03-29 23:33:31 +0530

    refactor : added router,provider,persistor

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/Store';
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
     <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+
+      <App />
+    </PersistGate>
     </Provider>
+    </Router>
   </React.StrictMode>
 );
 
