effd23242ba59e1c4bba0f4dac44ae68ef8894e4
Adi023 <adityapandhare050@gmail.com>
2024-03-29 23:28:41 +0530

    refactor : added reset role case

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,20 +1,35 @@
 // reducers.js
-import { SET_ROLE } from './actionTypes';
+import { SET_ROLE,RESET_ROLE } from './actionTypes';
 
 const initialState = {
-  role: 'default', // Default role is 'user'
+  role: 'default', // Default role is 'default'
 };
 
-const reducer = (state = initialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
+    //to set role for application
     case SET_ROLE:
+      console.log("SET_ROLE action received");
       return {
         ...state,
         role: action.payload,
       };
+      //to reset role for application
+      case RESET_ROLE:
+      return {
+        ...state,
+        role: initialState.role, // Reset role to default value
+      };
     default:
-      return state;
+      return state; // Return the current state without modifying it
   }
 };
 
-export default reducer;
+
+// Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes.
+      // Also, no return statement is required from these functions.
+
+// Action creators are generated for each case reducer function
