d52a6e1053ec017b4876aee71879e9517d3fa2ba
Adi023 <adityapandhare050@gmail.com>
2024-03-29 23:30:00 +0530

    feat : added reset role function

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,14 @@
 // actions.js
 import { SET_ROLE } from './actionTypes';
 
+
+// actions.js
+import { RESET_ROLE } from './actionTypes';
+
+export const resetRole = () => ({
+  type: RESET_ROLE,
+});
+
 export const setRole = (role) => ({
   type: SET_ROLE,
   payload: role,
