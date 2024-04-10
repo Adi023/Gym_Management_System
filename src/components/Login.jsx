b0e13f1f79d0c097d3640fa9e3c82ae7c002de85
Adi023 <adityapandhare050@gmail.com>
2024-04-10 17:46:06 +0530

    refactor : usestate modified

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,22 +4,29 @@ import UserLogin from './user/UserLogin';
 
 export default function Login() {
 
-    const [role, setRole] = useState(null);
-
+    const [role, setRole] = useState();
+   
+    
     const handleLogin = (userRole) => {
       setRole(userRole);
+      // localStorage.setItem('role', userRole);
+
     };
+    
+    // useEffect(() => {
+    //   const storedRole = localStorage.getItem('role');
+    //   if (storedRole) {
+    //     setRole(storedRole);
+    //   }
+    // }, []);
+    
 
   return (
-    <div>{role ? (
+    <div>{role? (
         <Dashboard role={role} />
       ) : (
         <UserLogin onLogin={handleLogin} />
       )}
-      
-      {/* {role?(
-        <Header role={role}/>
-      ):(<Header/>)} */}
       </div>
   )
 }
