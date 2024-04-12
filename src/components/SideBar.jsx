33426fdeb988ce0cf143645a8feaecd0cefadef8
Adi023 <adityapandhare050@gmail.com>
2024-04-12 17:39:52 +0530

    refactor : sidebar icon color changed made it responsive

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -14,76 +14,76 @@ export default function SideBar() {
   };
 
   return (
-    <div>
-      <div className="container-fluid">
+    <div >
+    <div className="container-fluid">
     <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div className="col-auto   px-0 bg-black">
             <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span className="fs-5 d-none d-sm-inline">Menu</span>
+                    <span className="custom-icon fs-5 d-none d-sm-inline">Menu</span>
                 </a>
                 <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li className="nav-item">
                         <a href="dashboard" className="nav-link align-middle px-0">
-                            <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
+                            <i className="fs-4 bi-house custom-icon " ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Home</span>
                         </a>
                     </li>
                     <li>
-                        <a href="/" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
+                        <a href="/register" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-speedometer2 custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Dashboard</span> </a>
                         <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
+                            <li className="w-100 ">
+                                <a href="/register" className="nav-link px-0 custom-icon"> <span className="custom-icon d-none d-sm-inline">Item</span> 1 </a>
                             </li>
                             <li>
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
+                                <a href="/" className="nav-link px-0 custom-icon"> <span className="custom-icon d-none d-sm-inline">Item</span> 2 </a>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <a href="/" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                            <i className="fs-4 bi-table custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Orders</span></a>
                     </li>
                     <li>
                         <a href="/" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                            <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                            <i className="fs-4 bi-bootstrap custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Bootstrap</span></a>
                         <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
                             <li className="w-100">
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Item</span> 1</a>
                             </li>
                             <li>
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Item</span> 2</a>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <a href="/submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
+                            <i className="fs-4 bi-grid custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Products</span> </a>
                            <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                             <li className="w-100">
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Product</span> 1</a>
                             </li>
                             <li>
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Product</span> 2</a>
                             </li>
                             <li>
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Product</span> 3</a>
                             </li>
                             <li>
-                                <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</a>
+                                <a href="/" className="nav-link px-0"> <span className="custom-icon d-none d-sm-inline">Product</span> 4</a>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <a href="/" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Customers</span> </a>
+                            <i className="fs-4 bi-people custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Customers</span> </a>
                     </li>
                 </ul>
                 <hr/>
                 <div className="dropdown pb-4">
                     <a href="/" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle"/>
-                        <span className="d-none d-sm-inline mx-1">loser</span>
+                        <span className="custom-icon d-none d-sm-inline mx-1">loser</span>
                     </a>
                     <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
                         <li><a className="dropdown-item" href="/">New project...</a></li>
@@ -97,11 +97,11 @@ export default function SideBar() {
                 </div>
             </div>
         </div>
-        {/* <div className="col py-3">
-            Content area...
-        </div> */}
+        {/* <div className="col py-3"> */}
+        {/* <Dashboard/> */}
+        {/* </div> */}
     </div>
 </div>
-    </div>
+</div>
   )
 }
