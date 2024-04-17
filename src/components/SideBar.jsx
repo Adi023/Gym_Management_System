1f740f62b0b6a1d50648e3d11ce9afcb633d7d31
Adi023 <adityapandhare050@gmail.com>
2024-04-18 00:01:48 +0530

    refactor : sidebar width css class added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -17,7 +17,7 @@ export default function SideBar() {
         <div >
             <div className="container-fluid">
                 <div className="row flex-nowrap">
-                    <div className="col-auto   px-0 bg-black">
+                    <div className=" sidewidth  px-0 bg-black" >
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                             <Link to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                                 <span className="custom-icon fs-5 d-none d-sm-inline">Menu</span>
@@ -36,12 +36,12 @@ export default function SideBar() {
                                         <span className="custom-icon ms-1 d-none d-sm-inline">User</span>
                                     </Link>
                                     <ul className="collapse  nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                        <li >
+                                        <li  className="w-100">
                                             <Link to="/register" className="nav-link px-0 custom-icon">
                                                 <i className=" bi bi-person-add "></i>
-                                                <span className="custom-icon d-none d-sm-inline"> Add </span></Link>
+                                                <span className="custom-icon d-none d-sm-inline"> Add</span></Link>
                                         </li>
-                                        <li>
+                                        <li className="w-100">
                                             <Link to="/viewUsers" className="nav-link px-0 custom-icon">
                                                 <i className=" bi bi-person-lines-fill "> </i>
                                                 <span className="custom-icon d-none d-sm-inline"> View</span></Link>
@@ -52,21 +52,22 @@ export default function SideBar() {
                                 {/* Attendance Fields */}
                                 <li>
                                     <Link to="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                                        <i className="fs-4 bi-table custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Attendance</span></Link>
-                                    
+                                        <i className="fs-4 bi-table custom-icon" ></i> 
+                                        <span className="custom-icon ms-1 d-none d-sm-inline">Attendance</span>    
+                                    </Link>
                                     <ul className="collapse  nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                                        <li >                                          
-                                                <Link to="/attendance" className="nav-link px-0 custom-icon">
+                                        <li className="w-100">
+                                            <Link to="/attendance" className="nav-link px-0 custom-icon">
                                                 <i className=" bi bi-person-add "></i>
-                                                <span className="custom-icon d-none d-sm-inline">Home</span>
-                                                </Link>
+                                                <span className="custom-icon d-none d-sm-inline"> Home</span>
+                                            </Link>
                                         </li>
                                         <li >
                                             <Link to="/markAttendance" className="nav-link px-0 custom-icon">
                                                 <i className=" bi bi-person-add "></i>
-                                                <span className="custom-icon d-none d-sm-inline"> Mark </span></Link>
+                                                <span className="custom-icon d-none d-sm-inline"> Mark</span></Link>
                                         </li>
-                                        <li>
+                                        <li >
                                             <Link to="/viewAttendance" className="nav-link px-0 custom-icon">
                                                 <i className=" bi bi-person-lines-fill "> </i>
                                                 <span className="custom-icon d-none d-sm-inline"> View</span></Link>
@@ -87,6 +88,7 @@ export default function SideBar() {
                                         </li>
                                     </ul>
                                 </li>
+
                                 <li>
                                     <Link to="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
                                         <i className="fs-4 bi-grid custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Products</span> </Link>
@@ -96,7 +98,7 @@ export default function SideBar() {
                                         <i className="fs-4 bi-people custom-icon" ></i> <span className="custom-icon ms-1 d-none d-sm-inline">Customers</span> </Link>
                                 </li>
                             </ul>
-                            <hr className="custom-icon"/>
+                            <hr className="custom-icon" />
                             <div className="dropdown pb-4">
                                 <Link to="/" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                                     {/* <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle"/> */}
