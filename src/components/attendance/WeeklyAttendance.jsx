c5c0f0fecc4e047a75210e435705f24063c9c03b
Adi023 <adityapandhare050@gmail.com>
2024-04-18 00:05:07 +0530

    refactor : unwanted imports removed

--- src/components/attendance/WeeklyAttendance.jsx
+++ src/components/attendance/WeeklyAttendance.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+import { Chart as _Chart} from 'chart.js/auto';
 import 'moment';
 import 'chartjs-adapter-moment';
 
