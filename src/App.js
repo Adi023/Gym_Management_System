b9af1d7c2f62c474c357b8fb817d0c7927c4e62b
Adi023 <adityapandhare050@gmail.com>
2024-04-18 00:05:30 +0530

    refactor : Modal added for sign out

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css'
 import Loading from "./components/Loading";
 import { useDispatch, useSelector } from "react-redux";
 import SideBar from "./components/SideBar";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+// import { Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { resetRole } from "./redux/actions";
+import { Button, Modal } from "react-bootstrap";
 
 function App() {
+  
   const [isLoading, setIsLoading] = useState(true);
   let appjsContent;
   useEffect(() => {
@@ -20,14 +22,29 @@ function App() {
       setIsLoading(false);
     }, 1000);
   }, []);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [showModal, setShowModal] = useState(false);
+
   const handleResetRole = () => {
+    setShowModal(true); // Show confirmation modal
+  };
+
+  const confirmSignOut = () => {
+    // Dispatch resetRole action and remove role from localStorage
     dispatch(resetRole());
     localStorage.removeItem('role');
     navigate('/');
+
+    setShowModal(false); // Hide modal after sign out
   };
+
+  const cancelSignOut = () => {
+    setShowModal(false); // Hide modal if sign out is canceled
+  };
+
   const role = useSelector(state => state.role);
   // console.log(role+"app");
 
@@ -37,20 +54,17 @@ function App() {
     case 'manager':
     case 'employee':
       appjsContent = (
-      <div className="d-flex">
-        <div className="sidebar">
-          <SideBar />
+        <div className="d-flex">
+          <div className="sidebar">
+            <SideBar />
+          </div>
+          <div className="content">
+            <nav className="navbar navbar-expand-sm bg-black navbar-dark mr-3 justify-content-end">
+              <button onClick={handleResetRole} className="btn btn-success ml-auto mr-3">SIGN OUT </button>
+            </nav>
+            <AllRoutes />
+          </div>
         </div>
-        
-        <div className="content">
-          <Navbar bg="black" variant="dark" expand="lg"  >
-            <Nav className="me-auto my-2 my-lg-0 align-items-start">
-              <Link onClick={handleResetRole} className="nav-link"><i className="bi bi-power"></i> LOGOUT</Link>
-            </Nav>
-          </Navbar>
-          <AllRoutes />
-        </div>
-      </div>
       )
       break;
     default:
@@ -63,8 +77,6 @@ function App() {
       break;
   }
   return (
-
-
     <div className="App">
       {isLoading ? (
         <Loading />
@@ -73,6 +85,20 @@ function App() {
           {appjsContent}
         </>
       )}
+      <Modal show={showModal} onHide={cancelSignOut}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Sign Out</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to sign out?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelSignOut}>
+            Cancel
+          </Button>
+          <Button variant="success" onClick={confirmSignOut}>
+            Sign Out
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
