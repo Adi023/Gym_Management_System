79c4486d7de5c21cce0f7143e930685a08f77ab6
Adi023 <adityapandhare050@gmail.com>
2024-04-18 17:37:13 +0530

    refactor : attendance services added

--- src/services/AttendanceServices.js
+++ src/services/AttendanceServices.js
@@ -1,7 +1,14 @@
-import http from '../http-common'
+import httpCommon from '../http-common'
 
 class AttendanceServices{
 
+    addAttendance(d){
+        return httpCommon.post("/attendance",d);
+    }
+
+    viewAttendanceData(){
+       return httpCommon.get("/attendance");
+    }
 }
 
 const myInstance = new AttendanceServices();
