8a2ae9eb2afaefcb8d8ef5c3c45d9502bebe05e6
Adi023 <adityapandhare050@gmail.com>
2024-04-22 00:36:56 +0530

    refactor : headers added

--- src/http-common.js
+++ src/http-common.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export default axios.create({
     baseURL : "http://localhost:5000/gym",
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      withCredentials: true,
 });
