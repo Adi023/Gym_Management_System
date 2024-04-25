9c2de91ff7b9bde95581fd0c51017e4cbe50146d
Adi023 <adityapandhare050@gmail.com>
2024-04-25 17:03:25 +0530

    refactor : reload button added

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 export default function ErrorPage() {
+ const reloadPage = () => {
+    window.location.reload();
+  };
   return (
-    <div><h1>Something went wrong.</h1></div>
+    <div className="container text-center mt-5">
+        <h1>Error</h1>
+        <p>Something went wrong.</p>
+        <button className="btn btn-primary" onClick={reloadPage}>
+          Reload Page
+        </button>
+      </div>
   )
 }
