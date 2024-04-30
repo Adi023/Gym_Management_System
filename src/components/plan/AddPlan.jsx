2a52a771c671c5c41b40bcfdafd9b087eb547344
Adi023 <adityapandhare050@gmail.com>
2024-05-01 01:00:40 +0530

    refactor: add plan form created

--- src/components/plan/AddPlan.jsx
+++ src/components/plan/AddPlan.jsx
@@ -1,7 +1,74 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import UserServices from '../../services/UserServices'
 
 export default function AddPlan() {
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const sendData = (d) => {
+      UserServices.addUser(d);
+      console.log(d);
+      alert("User Added Successfully")
+      reset();
+    }
   return (
-    <div>AddPlan</div>
+    <div className="signup-container">
+    <h2 style={{ marginLeft: '25%', position: 'relative' }}>Add New Plan</h2>
+      <form onSubmit={handleSubmit(sendData)}>
+      <div className="form-row">
+
+        <label htmlFor="planId">Plan Id</label>
+        <input type="text"  {...register('planId', { required: 'Plan Id Is Required' })} placeholder={"Plan Id"} />
+        {errors.planId && <p style={{ color: 'red' }}>{errors.planId.message}</p>}
+        <br />
+      
+        <label htmlFor="planName">Plan Name</label>
+        <input type="text"  {...register('planName', { required: 'Plan Name Is Required' })} placeholder={"Plan Name"} />
+        {errors.planName && <p style={{ color: 'red' }}>{errors.planName.message}</p>}
+        <br />
+    
+        <label htmlFor="Plan Description">Plan Price</label>
+        <input type="text"  {...register('planDescription', { required: 'Plan Description Is Required' })} placeholder={"Plan Description"} />
+        {errors.planDescription && <p style={{ color: 'red' }}>{errors.planDescription.message}</p>}
+        <br />
+    
+        <label htmlFor="planPrice">Plan Price</label>
+        <input type="text"  {...register('planPrice', { required: 'Plan Price Is Required' })} placeholder={"Plan Price"} />
+        {errors.planPrice && <p style={{ color: 'red' }}>{errors.planPrice.message}</p>}
+        <br />
+
+        <label>Billng Cycle</label>
+        <input type="number" {...register('planBillngCycle', { required: 'Billng Cycle Is Required' })} placeholder={"Billng Cycle"} />
+        {errors.planBillngCycle && <p style={{ color: 'red' }}>{errors.planBillngCycle.message}</p>}
+        <br />
+
+        <label>Plan Restrictions</label>
+        <input type="text" {...register('planRestrictions', { required: 'Plan Restrictions Is Required' })} placeholder={"Plan Restrictions"} />
+        {errors.planRestrictions && <p style={{ color: 'red' }}>{errors.planRestrictions.message}</p>}
+        <br />
+
+        <label>Plan Discount</label>
+        <input type="text" {...register('planDiscount', { required: 'Plan Discount Is Required' })} placeholder={"Plan Discount"} />
+        {errors.planDiscount && <p style={{ color: 'red' }}>{errors.planDiscount.message}</p>}
+        <br />
+
+        {/* <label>City</label>
+        <select {...register('city_Id', { required: 'City Is Required' })}>
+          <option value="">Select City</option>
+          <option value="1">MUMBAI</option>
+          <option value="2">NOIDA</option>
+        </select>
+        {errors.city_Id && <p style={{ color: 'red' }}>{errors.city_Id.message}</p>}
+        <br /> */}
+
+        <label></label>
+        <input type="submit" value={"Register"} placeholder={"Register"} />
+        <br/>
+        <label></label>
+        <input type="reset" value={"Reset"} placeholder={"Reset"} />
+        </div>
+      </form>
+    </div>
   )
 }
