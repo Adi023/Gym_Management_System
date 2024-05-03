4532c1aad50cbcb6b7617ce1c028be8c6cea7cf9
Adi023 <adityapandhare050@gmail.com>
2024-05-03 23:49:08 +0530

    refactor : is authanticated implemented on front end

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import  ErrorBoundary from '../ErrorBoundary'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ErrorBoundary from '../ErrorBoundary'
 
 import ErrorPage from './ErrorPage';
 
@@ -40,66 +40,87 @@ import Login from './Login';
 import Dashboard from './user/Dashboard';
 import ViewSingleUser from './user/ViewSingleUser';
 import ImageUploader from './ImageUploder';
+import { useSelector } from 'react-redux';
+import RequireAuth from './RequireAuth';
 
 
 
 
 export default function AllRoutes() {
+
+  const isAuthenticated = useSelector(state => state.role !== null && state.role !== 'default');
+console.log(isAuthenticated+"  isAuthenticated 1")
+
   return (
     <>
-        <div>
+      <div>
         {/* <Router> */}
-        <ErrorBoundary>
+        {/* <ErrorBoundary> */}
+
+
           <Routes>
-
-             {/* Error */}
-             <Route path='/ErrorBoundary' element={<ErrorBoundary/>}></Route>
-             <Route path='/ErrorPage' element={<ErrorPage/>}></Route>
-
-            {/*Admin Routes*/}
-            <Route path="Admin" element={<Admin></Admin>}></Route>
-            <Route path="AddAdmin" element={<AddAdmin></AddAdmin>}></Route>
-            <Route path="AdminLogin" element={<AdminLogin></AdminLogin>}></Route>
-            <Route path="UpdateAdmin" element={<UpdateAdmin></UpdateAdmin>}></Route>
-            <Route path="ViewAdmin" element={<ViewAdmin></ViewAdmin>}></Route>
-            <Route path="AdminDashBoard" element={<AdminDashBoard></AdminDashBoard>}></Route>
-
-            {/*Other Routes*/}
+            {/*Allowed to every one Routes*/}
             <Route path="/" element={<Home></Home>}></Route>
             <Route path="Header" element={<Header></Header>}></Route>
             <Route path="ContactUs" element={<ContactUs></ContactUs>}></Route>
             <Route path="AboutUs" element={<AboutUs></AboutUs>}></Route>
-
-            {/*User Routes*/}
             <Route path="/userlogin" element={<UserLogin></UserLogin>}></Route>
-            <Route path='/register' element={<AddUser></AddUser>}></Route>
-            <Route path='/viewUsers' element={<ViewAllUsers></ViewAllUsers>}></Route>
-            <Route path='/viewSingleUser' element={<ViewSingleUser></ViewSingleUser>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
-            <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
 
-            {/* Attendance Routes */}
-            <Route path='/attendance' element={<AttendanceHome></AttendanceHome>}></Route>
-            <Route path='/weeklyAttendance' element={<WeeklyAttendance/>}></Route>
-            <Route path='/markAttendance' element={<MarkAttendance/>}></Route>
-            <Route path='/viewAttendance' element={<ViewAttendance/>}></Route>
+            {/* Error */}
+            <Route path='/ErrorBoundary' element={<ErrorBoundary />}></Route>
+            <Route path='/ErrorPage' element={<ErrorPage />}></Route>
+            <Route path="/unauthorized" element={<div>Unauthorized Access!</div>} />
 
-            {/* Employee Routes */}
-            <Route path='/addEmployee' element={<AddEmployee/>} ></Route>
-            <Route path='/viewEmployee' element={<ViewEmployee/>} ></Route>
+            {/* Only for AUthenticated and autherized users */}
+            {isAuthenticated ? (
+              <>
+                {/*Admin Routes*/}
+                <Route path="/Admin" element={<RequireAuth component={Admin} requiredRoles={['admin']} />} />
+                <Route path="register" element={<RequireAuth component={AddUser}  /> }  />
 
-            {/* Plan Routes */}
-            <Route path='/addPlan' element={<AddPlan/>}></Route>
-            <Route path='/viewPlan' element={<ViewPlan/>}></Route>
-            <Route path='/updatePlan' element={<UpdatePlan/>}></Route>
+                {/* <Route path="Admin" element={<RequireAuth component={<Admin/>} requiredRoles={['admin']} />}  /> */}
+                <Route path="AddAdmin" element={<RequireAuth component={AddAdmin} requiredRoles={['admin']} />}  />
+                <Route path="AdminLogin" element={<RequireAuth component={AdminLogin} requiredRoles={['admin']} />}  />
+                <Route path="UpdateAdmin" element={<RequireAuth component={UpdateAdmin} requiredRoles={['admin']} />}  />
+                <Route path="ViewAdmin" element={<RequireAuth component={ViewAdmin} requiredRoles={['admin']} />}  />
+                <Route path="AdminDashBoard" element={<RequireAuth component={AdminDashBoard} requiredRoles={['admin']} />}  />
 
-            {/* Helper Routes */}
-           <Route path='/imageUploader' element={<ImageUploader/>}></Route>
-            
+                {/*User Routes*/}
+                {/* <Route path='/register' element={<RequireAuth component={AddUser} requiredRoles={['admin']} />}  /> */}
+                <Route path='/viewUsers' element={<RequireAuth component={ViewAllUsers} requiredRoles={['admin']} />}  />
+                <Route path='/viewSingleUser' element={<RequireAuth component={ViewSingleUser} requiredRoles={['admin']} />}  />
+                <Route path='/dashboard' element={<RequireAuth component={Dashboard} requiredRoles={['admin']} />}  />
+
+                {/* Attendance Routes */}
+                <Route path='attendance' element={<RequireAuth component={AttendanceHome} requiredRoles={['admin']} />}  />
+                <Route path='weeklyAttendance' element={<RequireAuth component={WeeklyAttendance} requiredRoles={['admin']} />}  />
+                <Route path='markAttendance' element={<RequireAuth component={MarkAttendance} requiredRoles={['admin']} />}  />
+                <Route path='viewAttendance' element={<RequireAuth component={ViewAttendance} requiredRoles={['admin']} />}  />
+
+                {/* Employee Routes */}
+                <Route path='addEmployee' element={<RequireAuth component={AddEmployee}  requiredRoles={['admin']} />}  />
+                <Route path='viewEmployee' element={<RequireAuth component={ViewEmployee}  requiredRoles={['admin']} />}  />
+
+                {/* Plan Routes */}
+                <Route path='addPlan' element={<RequireAuth component={AddPlan} requiredRoles={['admin']} />}  />
+                <Route path='viewPlan' element={<RequireAuth component={ViewPlan} requiredRoles={['admin']} />}  />
+                <Route path='updatePlan' element={<RequireAuth component={UpdatePlan} requiredRoles={['admin']} />}  />
+
+                {/* Helper Routes */}
+                <Route path='imageUploader' element={<RequireAuth component={ImageUploader} requiredRoles={['admin']} />}  />
+
+              </>
+            ) : (
+              
+              <Route path="/" element={<div>Unauthorized Access!</div>} />
+
+            ) }
+
           </Routes>
-          </ErrorBoundary>
-       {/* </Router> */}
-        </div>
+        {/* </ErrorBoundary> */}
+        {/* </Router> */}
+      </div>
     </>
   )
 }
