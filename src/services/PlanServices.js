7ca30312cc2a13a4218991f8f00746166cc09718
Adi023 <adityapandhare050@gmail.com>
2024-05-05 00:38:24 +0530

    refactor : services added for add edit view delete

--- src/services/PlanServices.js
+++ src/services/PlanServices.js
@@ -1,7 +1,26 @@
-import http from '../http-common'
+import httpCommon from '../http-common';
 
 class PlanServices{
 
+    addPlan(d){
+        return httpCommon.post("/plan",d);
+    }
+    viewPlan(){
+        return httpCommon.get("/plan");
+    }
+
+    viewAllPlan(pageNumber,pageSize,sortBy,sortDir){
+        // console.log(`Sort By: ${sortBy}, Sort Dir: ${sortDir}`);
+        return httpCommon.get(`/plan/${pageNumber}/${pageSize}/${sortBy}/${sortDir}`);
+    }
+
+    getPlanById(planId){
+        return httpCommon.get("/plan/"+planId)
+    }
+
+    deletePlan(planId){
+        return httpCommon.delete("/plan/"+planId)
+    }
 }
 
 const myInstance = new PlanServices();
