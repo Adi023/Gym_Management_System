2aa6bce61e326fd1038bd2e1da150fec55e93275
Adi023 <adityapandhare050@gmail.com>
2024-05-07 00:31:00 +0530

    refactor : id removed as becouse auto genarated

--- src/components/plan/AddPlan.jsx
+++ src/components/plan/AddPlan.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
-import PlanServices from '../../services/PlanServices'
+import PlanServices from '../../services/PlanServices';
+import { toast } from 'react-toastify';
 
 export default function AddPlan() {
 
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
-    const sendData = (d) => {
-      PlanServices.addPlan(d);
-      console.log(d);
-      alert("Plan Added Successfully")
-      reset();
+  const sendData = async (d) => {
+    try {
+      // Assuming PlanServices.addPlan returns the ID of the newly added plan
+      const data = await PlanServices.addPlan(d);
+      
+      // console.log(data.data.planId);
+      // console.log("New plan ID:", data.data.planId);
+      
+      toast.success(`Plan Added Successfully, ID: ${data.data.planId}`, {
+        autoClose: false,
+       
+      });
+      
+      reset(); // Assuming reset function is defined somewhere
+    } catch (error) {
+      console.error("Error adding plan:", error);
+      toast.error("Failed to add plan");
     }
+  }
   return (
     <div className="signup-container">
     <h2 style={{ marginLeft: '25%', position: 'relative' }}>Add New Plan</h2>
       <form onSubmit={handleSubmit(sendData)}>
       <div className="form-row">
 
-        <label htmlFor="planId">Plan Id</label>
+        {/* <label htmlFor="planId">Plan Id</label>
         <input type="text"  {...register('planId', { required: 'Plan Id Is Required' })} placeholder={"Plan Id"} />
         {errors.planId && <p style={{ color: 'red' }}>{errors.planId.message}</p>}
-        <br />
+        <br /> */}
       
         <label htmlFor="planName">Plan Name</label>
         <input type="text"  {...register('planName', { required: 'Plan Name Is Required' })} placeholder={"Plan Name"} />
