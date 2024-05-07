ca1e059fc11f264dfb287a32f9a705d7d4b2132e
Adi023 <adityapandhare050@gmail.com>
2024-05-07 23:22:41 +0530

    refactor : code added

--- src/components/membership/AddMembership.jsx
+++ src/components/membership/AddMembership.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+import MembershipServices from '../../services/MembershipServices'
+import PlanServices from '../../services/PlanServices'
+
 
 export default function AddMembership() {
+  const [minDate, setMinDate] = useState('');
+  const [planData, setPlanData] = useState({
+    content: [],
+    totalPages: '',
+    totalElements: '',
+    pageSize: '',
+    lastPage: false,
+    pageNumber: 0
+  });
+  useEffect(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+
+    // Add leading zeros if month or day is less than 10
+    if (month < 10) {
+      month = '0' + month;
+    }
+    if (day < 10) {
+      day = '0' + day;
+    }
+
+    const formattedDate = `${year}-${month}-${day}`;
+    setMinDate(formattedDate);
+
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const responseData = await PlanServices.viewAllPlan(0, 50, "planId", "asc");
+      console.log(responseData);
+      // toast.success("Succses")
+      setPlanData(responseData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+  const sendData = (d) => {
+    MembershipServices.addMembership(d);
+    console.log(d);
+    alert("User Added Successfully")
+    reset();
+  }
+
   return (
-    <div>AddMembership</div>
+    <div className="signup-container">
+      <h2 style={{ marginLeft: '25%', position: 'relative' }}>Add New Membership</h2>
+      <form onSubmit={handleSubmit(sendData)} autocomplete="nope">
+
+        <div className="form-row">
+
+          <label htmlFor="inputUserId">User Id</label>
+          <input type="text"  {...register('userId', { required: 'User Id Is Required' })} placeholder={"User Id"} autocomplete="new-password" />
+          {errors.userId && <p style={{ color: 'red' }}>{errors.userId.message}</p>}
+          <br />
+
+          <label>Plan</label>
+          <select {...register('planId', { required: 'Plan Is Required' })}>
+            <option value="">Select Plan</option>
+            {planData.content.map(plan => (
+              <option key={plan.planId} value={plan.planId}>{plan.planName}</option>
+            ))}
+          </select>
+          {errors.planId && <p style={{ color: 'red' }}>{errors.planId.message}</p>}
+          <br />
+
+          <label>Start Date</label>
+          <input type="date" {...register('membershipStartDate')}
+            placeholder={"Start Date"}
+            min={minDate}
+          />
+          {errors.membershipStartDate && <p style={{ color: 'red' }}>{errors.membershipStartDate.message}</p>}
+          <br />
+
+          <label>End Date</label>
+          <input type="date" {...register('membershipEndDate')}
+            placeholder={"End Date"}
+            min={minDate}
+          />
+          {errors.membershipEndDate && <p style={{ color: 'red' }}>{errors.membershipEndDate.message}</p>}
+          <br />
+
+          <label></label>
+          <input type="submit" value={"Register"} placeholder={"Register"} />
+          <br />
+          <label></label>
+          <input type="reset" value={"Reset"} placeholder={"Reset"} />
+
+        </div>
+      </form>
+
+    </div>
   )
 }
--- src/components/membership/ViewMemberships.jsx
+++ src/components/membership/ViewMemberships.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import MembershipServices from '../../services/MembershipServices'
+import PaginationBar from '../PaginationBar';
 
 export default function ViewMemberships() {
+  const [pageSize, setPageSize] = useState(5);
+  const [sortBy, setSortBy] = useState('membershipId');
+  const [sortDir, setSortDir] = useState('desc');
+  const [searchTerm, setSearchTerm] = useState("")
+  const [postContent, setPostContent] = useState({
+    content: [],
+    totalPages: '',
+    totalElements: '',
+    pageSize: '',
+    lastPage: false,
+    pageNumber: 0
+  });
+
+  useEffect(() => {
+    fetchData();
+    // toast.success("Succses")
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const responseData = await MembershipServices.viewAllMembership(0, 5, "membershipId", "asc");
+      console.log(responseData);
+      // toast.success("Succses")
+      setPostContent(responseData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handlePageChange = async (pageNumber = 0) => {
+    try {
+      const responseData = await  MembershipServices.viewAllMembership(pageNumber, pageSize, sortBy, sortDir);
+      console.log(responseData);
+      setPostContent(responseData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSortChange = (e) => {
+    setSortDir(e); // Update the sorting direction
+    handlePageChange(); 
+  }
+
+  const handleSortBy = (e) => {
+    setSortBy(e);
+    handlePageChange();
+  }
+
   return (
-    <div>
-      
+    <div className='container '>
+      <br />
+      <div className="row m-0 p-0 ">
+        <h1>View Memberships</h1>
+      </div>
+      <br />
+
+      <div className="row m-0 p-0 divcardProfile">
+        <div className="col  col-md-4 ms-auto m-0 p-0" style={{ width: '550px' }}>
+
+          <label htmlFor="searchInput" className="col-auto">Search:</label>
+          <input type="text" id="searchInput" className="col col-auto  py-0 pl-1"
+            style={{ width: '280px', marginRight: '15px' }}
+            placeholder="Enter Id, User Id, or Attendance Date" onChange={event => { setSearchTerm(event.target.value) }} />
+
+          <label htmlFor="pageSizeInput" className=' col-auto' >Page Size:</label>
+          <input
+            type="number"
+            id="pageSizeInput"
+            className=" col col-auto m-0  py-0 pl-1 "
+            style={{ width: '100px' }}
+            value={pageSize}
+            placeholder="Enter page size"
+            onChange={(e) => {
+              const pageSize = parseInt(e.target.value.trim());
+              if (pageSize > 0 && pageSize <= postContent.totalElements) {
+                setPageSize(pageSize);
+              } else {
+                if (pageSize === ' ') {
+                  setPageSize('');
+                } else {
+                  setPageSize('');
+                  alert("Enter Valid Page Size");
+                }
+              }
+            }}
+          />
+        </div>
+
+        <div className="col-auto m-0 p-0" >
+          <button className="btn m-0 p-0" onClick={() => handleSortChange('asc')}>
+            <i className="bi bi-sort-up fa-2x"></i>
+          </button>
+          <button className="btn m-0 p-0" onClick={() => handleSortChange('desc')}>
+            <i className="bi bi-sort-down-alt fa-2x"></i>
+          </button>
+        </div>
+      </div><br />
+
+      <div className="table-responsive divcardProfile" >
+        {/* className="table-responsive" */}
+        <table className="table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col" onClick={() => handleSortBy('membershipId')}>Id</th>
+              <th scope="col" onClick={() => handleSortBy('userId')}>User Name</th>
+              <th scope="col" onClick={() => handleSortBy('planId')}>Plan Name</th>
+              <th scope="col" onClick={() => handleSortBy('membershipStartDate')}>Start Date</th>
+              <th scope="col" onClick={() => handleSortBy('membershipEndDate')}>End Date</th>
+              <th scope="col" onClick={() => handleSortBy('ts')}>Added Date</th>ss
+            </tr>
+          </thead>
+          <tbody>
+            {postContent.content.filter((d) => {
+              if (searchTerm === "") {
+                return d;
+              }
+              else if (String(d.membershipId).toLowerCase().includes(String(searchTerm).toLowerCase())) {
+                return d;
+              }
+              else if (d.planName.toLowerCase().includes(String(searchTerm).toLowerCase())) {
+                return d;
+              }
+            }).map((d) => (
+              <tr key={d.membershipId}>
+                <td data-label="Id : ">{d.membershipId}</td>
+                <td data-label="User Name  : ">{d.user?.name}</td>
+                <td data-label="Plan Name :  ">{d.plan?.planName}</td>
+                <td data-label="Start Date : ">{d.membershipStartDate}</td>
+                <td data-label="End Date : ">{d.membershipEndDate}</td>
+                <td data-label="ts : ">{new Date(d.ts).toLocaleDateString()}</td>      
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <br/>
+      {/* Pagination Bar Starts*/}
+      <div className="row m-0 p-0 divcardProfile">
+      <PaginationBar postContent={postContent} handlePageChange={handlePageChange} />
+     </div>
     </div>
   )
 }
