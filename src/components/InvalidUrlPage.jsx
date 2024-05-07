df6620ee70427dae3a30ad37aa89d7365922e778
Adi023 <adityapandhare050@gmail.com>
2024-05-07 20:38:13 +0530

    refactor : design changes

--- src/components/InvalidUrlPage.jsx
+++ src/components/InvalidUrlPage.jsx
@@ -8,31 +8,16 @@ export default function InvalidUrlPage() {
         navigate("/");
         window.location.reload();
        
-    };const [isOpen, setIsOpen] = useState(false);
-
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
     };
 
     return (
-        // <div className="container text-center mt-5">
-        //     <h1>Error</h1>
-        //     <p>Invalid Url..!!</p>
-        //     <button className="btn btn-primary" onClick={reloadPage}>
-        //         Go To Home
-        //     </button>
-        // </div>
-        <>
-         <div>
-            {/* Link to toggle dropdown */}
-            <Link to="/" onClick={toggleSidebar}>Toggle Dropdown</Link>
-            
-            {/* Dropdown menu */}
-            <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
-                <li><Link to="/">Option 1</Link></li>
-                <li><Link to="/">Option 2</Link></li>
-            </ul>
+        <div className="container text-center mt-5">
+            <h1>Error</h1>
+            <p>Invalid Url..!!</p>
+            <button className="btn btn-primary" onClick={reloadPage}>
+                Go To Home
+            </button>
         </div>
-        </>
+        
     );
 }
