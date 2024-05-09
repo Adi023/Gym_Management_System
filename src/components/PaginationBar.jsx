c126cac49de081ddb3a10dfa73606d1e384de99e
Adi023 <adityapandhare050@gmail.com>
2024-05-09 22:44:21 +0530

    refactor : corrected numbering

--- src/components/PaginationBar.jsx
+++ src/components/PaginationBar.jsx
@@ -4,24 +4,27 @@ import { Container, Pagination, PaginationItem, PaginationLink } from 'reactstra
 export default function PaginationBar({ postContent, handlePageChange }) {
     const startPage = Math.max(0, postContent.pageNumber - 1);
     const endPage = Math.min(postContent.totalPages - 1, startPage + 2);
-    
     return (
         <div className='row'>
             <Container className='mt-3'>
-                <Pagination aria-label="Page navigation example" className='col-md-4 ms-auto' size="md">
-                    <PaginationItem >
-                        <PaginationLink onClick={() => handlePageChange(0)} disabled={postContent.pageNumber === 0}>
+                <Pagination aria-label="Page navigation example" className='col-md-4 ms-auto' size="sm">
+                    <PaginationItem disabled={postContent.pageNumber === 0}>
+                        <PaginationLink onClick={() => handlePageChange(0)} >
                             First
                         </PaginationLink>
                     </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink onClick={() => handlePageChange(postContent.pageNumber - 1)} previous disabled={postContent.pageNumber === 0}>
+                    <PaginationItem disabled={postContent.pageNumber === 0}>
+                        <PaginationLink onClick={() => handlePageChange(postContent.pageNumber - 1)} previous >
                             Previous
                         </PaginationLink>
                     </PaginationItem>
                     {/* Start */}
 
-
+                    {postContent.pageNumber > 1 && postContent.pageNumber < 2 && (
+                        <PaginationItem>
+                            <PaginationLink>1</PaginationLink>
+                        </PaginationItem>
+                    )}
                     {postContent.pageNumber > 1 && (
                         <PaginationItem disabled>
                             <PaginationLink>...</PaginationLink>
@@ -45,21 +48,21 @@ export default function PaginationBar({ postContent, handlePageChange }) {
                         </PaginationItem>
                     )}
 
-                    {postContent.pageNumber + 2 < postContent.totalPages && (
-                        <PaginationItem >
-                            <PaginationLink onClick={() => handlePageChange(postContent.totalPages - 1)} disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                    {(postContent.pageNumber + 2 < postContent.totalPages) && (
+                        <PaginationItem disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                            <PaginationLink onClick={() => handlePageChange(postContent.totalPages - 1)} >
                                 {postContent.totalPages}</PaginationLink>
                         </PaginationItem>
                     )}
 
                     {/* end */}
-                    <PaginationItem>
-                        <PaginationLink onClick={() => handlePageChange(postContent.pageNumber + 1)} next disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                    <PaginationItem disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                        <PaginationLink onClick={() => handlePageChange(postContent.pageNumber + 1)} next >
                             Next
                         </PaginationLink>
                     </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink onClick={() => handlePageChange(postContent.totalPages - 1)} disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                    <PaginationItem disabled={postContent.pageNumber === postContent.totalPages - 1}>
+                        <PaginationLink onClick={() => handlePageChange(postContent.totalPages - 1)} >
                             Last
                         </PaginationLink>
                     </PaginationItem>
