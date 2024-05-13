05382dd621d179b111910af1330c4910b97ef586
Adi023 <adityapandhare050@gmail.com>
2024-05-13 23:17:33 +0530

    refactor : time made darker

--- src/components/Helper/DigitalClock.jsx
+++ src/components/Helper/DigitalClock.jsx
@@ -24,18 +24,22 @@ export default function DigitalClock() {
 
               <div className="clock-item">
                 <p className="clock-label mx-2">Date</p>
-                <p className="clock-day clock-timer">{dateTime.toLocaleDateString("es-CL")}</p>
+                <p className="clock-day clock-timer"><h4>{dateTime.toLocaleDateString("es-CL")}</h4></p>
               </div>
+
+
               <div className="clock-item">
-                <p className="clock-hours clock-timer">{dateTime.getHours()}</p>
+                <p className="clock-hours clock-timer"><h4>{dateTime.getHours()}</h4></p>
                 <p className="clock-label mr-4 mx-4">Hours</p>
               </div>
+              <p className="clock-timer mx-2">:</p>
               <div className="clock-item">
-                <p className="clock-minutes clock-timer">{dateTime.getMinutes()}</p>
+                <p className="clock-minutes clock-timer"><h4>{dateTime.getMinutes()}</h4></p>
                 <p className="clock-label mx-2">Minutes</p>
               </div>
+              <p className="clock-timer mx-2">:</p>
               <div className="clock-item">
-                <p className="clock-seconds clock-timer">{dateTime.getSeconds()}</p>
+                <p className="clock-seconds clock-timer"><h4>{dateTime.getSeconds()}</h4></p>
                 <p className="clock-label mx-2">Seconds</p>
               </div>
             </div>
