b19d195b8ab45ad39cff07edae8dd40eb62c3750
Adi023 <adityapandhare050@gmail.com>
2024-05-14 22:56:19 +0530

    refactor : icon added navigation fixed

--- src/components/InvalidUrlPage.jsx
+++ src/components/InvalidUrlPage.jsx
@@ -12,12 +12,19 @@ export default function InvalidUrlPage() {
 
     return (
         <div className="container text-center mt-5">
-            <h1>Error</h1>
-            <p>Invalid Url..!!</p>
-            <button className="btn btn-primary" onClick={reloadPage}>
-                Go To Home
-            </button>
+        {/* <h1>Error</h1> */}
+        <i class="bi bi-exclamation-triangle-fill" style={{fontSize:"150px"}} ></i>
+        <div>
+          <h1>Invalid Url</h1>
+          <p>This url is Invalid. Please <Link to="/" onClick={reloadPage}>go back</Link> to the home page.</p>
         </div>
+        <p>Or</p>
+        
+        <button className="btn btn-primary" onClick={reloadPage}>
+                Go To Home
+        </button>
+        
+    </div>
         
     );
 }
