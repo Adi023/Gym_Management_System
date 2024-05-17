75d27df49f7d548ff16b8f7fbd2baa570a93fd10
Adi023 <adityapandhare050@gmail.com>
2024-05-17 22:28:56 +0530

    refactor : Footer re-designed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,15 @@ import React from 'react'
 export default function Footer() {
     return (
         <div>
-            {/* <!-- Footer --> */}
-            <footer className="text-center text-lg-start bg-body-tertiary text-muted">
-                {/* <!-- Section: Social media --> */}
+            <footer className="text-center text-lg-start bg-body-tertiary text-muted" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
                 <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-                    {/* <!-- Left --> */}
-                    <div className="me-5 d-none d-lg-block">
-                        <span>Get connected with us on social networks:</span>
-                    </div>
-                    {/* <!-- Left --> */}
 
-                    {/* <!-- Right --> */}
+                    <div className="me-5 d-none d-lg-block">
+                        <div className="text-center p-4" >
+                            © 2024 Copyright:
+                            <a className="text-reset fw-bold" href="/">GMS.com</a> </div>
+                    </div>
+
                     <div>
                         <a href="/" className="me-4 text-reset">
                             <i className="fab fa-facebook-f"></i>
@@ -34,97 +32,9 @@ export default function Footer() {
                             <i className="fab fa-github"></i>
                         </a>
                     </div>
-                    {/* <!-- Right --> */}
                 </section>
-                {/* <!-- Section: Social media --> */}
-
-                {/* <!-- Section: Links  --> */}
-                <section className="">
-                    <div className="container text-center text-md-start mt-5">
-                        {/* <!-- Grid row --> */}
-                        <div className="row mt-3">
-                            {/* <!-- Grid column --> */}
-                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                                {/* <!-- Content --> */}
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    <i className="fas fa-gem me-3"></i>Gym name
-                                </h6>
-                                <p>
-                                    Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                    dolor sit amet, consectetur adipisicing elit.
-                                </p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                               
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    Products
-                                </h6>
-                                <p>
-                                    <a href="/" className="text-reset">Home</a>
-                                </p>
-                                <p>
-                                    <a href="/AboutUs" className="text-reset">About us</a>
-                                </p>
-                                <p>
-                                    <a href="/ContactUs" className="text-reset">Contact Us</a>
-                                </p>
-                                {/* <p>
-                                    <a href="/" className="text-reset">Laravel</a>
-                                </p> */}
-                            </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                {/* <!-- Links --> */}
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    Useful links
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Pricing</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Settings</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Orders</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Help</a>
-                                </p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                                {/* <!-- Links --> */}
-                                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                                <p><i className="fas fa-home me-3"></i> India </p>
-                                <p>
-                                    <i className="fas fa-envelope me-3"></i>
-                                    info@gms.com
-                                </p>
-                                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-                        </div>
-                        {/* <!-- Grid row --> */}
-                    </div>
-                </section>
-                {/* <!-- Section: Links  --> */}
-
-                {/* <!-- Copyright --> */}
-                <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-                    © 2024 Copyright:
-                    <a className="text-reset fw-bold" href="https://mdbootstrap.com/">GMS.com</a>
-                </div>
-                {/* <!-- Copyright --> */}
             </footer>
-            {/* <!-- Footer --> */}
+            <br/>
         </div>
     )
 }
