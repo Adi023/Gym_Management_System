93a0c90601a3dd90715752a103562ddbe3437604
Adi023 <adityapandhare050@gmail.com>
2024-05-27 23:39:58 +0530

    refactor : user sidebar componente added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -167,38 +167,26 @@ export default function SideBar() {
 
                 {/* User Fields */}
                 <li>
-                            <Link to="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <Link to="/viewSingleUser" className="nav-link px-0 align-middle">
                                 <i className="fs-4 bi bi-people custom-icon"></i>
                                 <span className="custom-icon ms-1 d-none d-sm-inline">Profile</span>
                             </Link>
-                            <ul className="collapse  nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                <li className="w-100">
-                                    <Link to="/register" className="nav-link px-0 text-secondary text-hover-white">
-                                        <i className=" bi bi-person-add "></i>
-                                        <span className="d-none d-sm-inline"> Add</span></Link>
-                                </li>
-                                {/* <li className="w-100">
-                                    <Link to="/viewUsers" className="nav-link px-0 text-secondary text-hover-white">
-                                    <i className="fa-solid fa-list"></i>
-                                        <span className="d-none d-sm-inline"> View</span></Link>
-                                </li> */}
-                            </ul>
-                           
                 </li>
                 <hr style={{margin:'0px',color:'white',width:'100%'}}/>
 
                 <li>
-                            <Link to="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <Link to="/viewMembershipById"  className="nav-link px-0 align-middle">
                                 <i className="fs-4 bi bi-people custom-icon"></i>
                                 <span className="custom-icon ms-1 d-none d-sm-inline">Membership</span>
                             </Link>
-                            <ul className="collapse  nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                                <li className="w-100">
-                                    <Link to="/" className="nav-link px-0 text-secondary text-hover-white">
-                                        <i className=" bi bi-person-add "></i>
-                                        <span className="d-none d-sm-inline">Details</span></Link>
-                                </li>
-                            </ul>
+                </li>
+                <hr style={{margin:'0px',color:'white',width:'100%'}}/>
+
+                <li>
+                            <Link to="/attendanceByUserId"  className="nav-link px-0 align-middle">
+                                <i className="fs-4 bi bi-people custom-icon"></i>
+                                <span className="custom-icon ms-1 d-none d-sm-inline">Attendance</span>
+                            </Link>
                 </li>
                 <hr style={{margin:'0px',color:'white',width:'100%'}}/>
                 </>
