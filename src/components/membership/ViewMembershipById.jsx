da276427a48f1a73b24c13166a3432b4513ae3ec
Adi023 <adityapandhare050@gmail.com>
2024-05-29 00:09:03 +0530

    refacctor : serch bar and status column added

--- src/components/membership/ViewMembershipById.jsx
+++ src/components/membership/ViewMembershipById.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import MembershipServices from '../../services/MembershipServices'
 import { useSelector } from 'react-redux';
+import TodaysDate from '../Helper/TodaysDate'
 
 
 export default function ViewMembershipById() {
@@ -8,12 +9,12 @@ export default function ViewMembershipById() {
   const [searchTerm, setSearchTerm] = useState("")
   const [postContent, setPostContent] = useState([]);
   const userId = useSelector(state => state.userId);
- 
+
 
   const fetchData = async () => {
     try {
       const responseData = await MembershipServices.getMembershipByUserId(userId);
-      // console.log(responseData);
+      console.log(responseData);
       // toast.success("Succses")
       setPostContent(responseData.data);
     } catch (error) {
@@ -26,7 +27,6 @@ export default function ViewMembershipById() {
     // toast.success("Succses")
   }, []);
 
-
   return (
     <div className='container '>
       <br />
@@ -34,7 +34,15 @@ export default function ViewMembershipById() {
         <h1>View Membership</h1>
       </div>
       <br />
-
+      <div className="row m-0 p-0 divcardProfile">
+        <div className="col  col-md-4 ms-auto m-0 p-0" style={{ width: '550px' }}>
+          <label htmlFor="searchInput" className="col-auto">Search:</label>
+          <input type="text" id="searchInput" className="col col-auto  py-0 pl-1"
+            style={{ width: '280px', marginRight: '15px' }}
+            placeholder="Attendance Date or Attendance Id" onChange={event => { setSearchTerm(event.target.value) }} />
+        </div>
+      </div>
+      <br />
       <div className="table-responsive divcardProfile" >
         {/* className="table-responsive" */}
         <table className="table table-hover">
@@ -45,7 +53,8 @@ export default function ViewMembershipById() {
               <th >Plan Name</th>
               <th >Start Date</th>
               <th >End Date</th>
-              <th >Added Date</th>ss
+              <th >Added Date</th>
+              <th >Status</th>
             </tr>
           </thead>
           <tbody>
@@ -56,7 +65,7 @@ export default function ViewMembershipById() {
               else if (String(d.membershipId).toLowerCase().includes(String(searchTerm).toLowerCase())) {
                 return d;
               }
-              else if (d.planName.toLowerCase().includes(String(searchTerm).toLowerCase())) {
+              else if (d.plan.planName.toLowerCase().includes(String(searchTerm).toLowerCase())) {
                 return d;
               }
             }).map((d) => (
@@ -66,14 +75,16 @@ export default function ViewMembershipById() {
                 <td data-label="Plan Name :  ">{d.plan?.planName}</td>
                 <td data-label="Start Date : ">{d.membershipStartDate}</td>
                 <td data-label="End Date : ">{d.membershipEndDate}</td>
-                <td data-label="ts : ">{new Date(d.ts).toLocaleDateString()}</td>      
+                <td data-label="Added Date : ">{new Date(d.ts).toLocaleDateString()}</td>
+                <td data-label="Status : ">{(d.membershipEndDate < TodaysDate.getDate()) ?
+                  <h6 style={{ color: "Red" }}>Expired</h6> : <h6 style={{ color: "green" }}>Active</h6>}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <br/>
-    
+      <br />
+
     </div>
   )
 }
