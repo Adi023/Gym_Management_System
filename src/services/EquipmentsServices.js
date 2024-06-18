303b0362dc07c5c9a3d0d05de68b571f8942ee15
Adi023 <adityapandhare050@gmail.com>
2024-06-19 00:46:15 +0530

    refactor : path name corrected

--- src/services/EquipmentsServices.js
+++ src/services/EquipmentsServices.js
@@ -3,23 +3,23 @@ import httpCommon from "../http-common";
 class EquipementsServices{
 
     addEquipements(d){
-       return  httpCommon.post("/equipement",d);
+       return  httpCommon.post("/equipment",d);
     }
 
     viewEquipements(pageNumber,pageSize,sortBy,sortDir){
-        return httpCommon.get(`/equipement/${pageNumber}/${pageSize}/${sortBy}/${sortDir}`);
+        return httpCommon.get(`/equipment/${pageNumber}/${pageSize}/${sortBy}/${sortDir}`);
     }
 
     viewEquipementById(eqpId){
-        return httpCommon.get("/equipement/"+eqpId);
+        return httpCommon.get("/equipment/"+eqpId);
     }
 
     updateEquipementDetails(d){
-        return httpCommon.put("/equipement/{eqpId}",d);
+        return httpCommon.put("/equipment/{eqpId}",d);
     }
 
     deleteEquipement(eqpId){
-        return httpCommon.delete("/equipement/"+eqpId);
+        return httpCommon.delete("/equipment/"+eqpId);
     }
 }
 const myInstance =new EquipementsServices();
