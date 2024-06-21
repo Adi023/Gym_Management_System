6355e05f9de396998dcc239f54c41fd458e8baac
Adi023 <adityapandhare050@gmail.com>
2024-06-21 17:23:03 +0530

    refactor : active field value added as default

--- src/components/activities/AddActivity.jsx
+++ src/components/activities/AddActivity.jsx
@@ -46,10 +46,9 @@ export default function AddActivity() {
         {errors.activityCreatedBy && <p style={{ color: 'red' }}>{errors.activityCreatedBy.message}</p>}
         <br />
 
-        <label>Activity isActive</label>
-        <input type="text" {...register('isActive', { required: 'Activity Restrictions Is Required' })} placeholder={"Activity is Active"} />
+        {/* <label>Activity isActive</label> */}
+        <input type="hidden" {...register('isActive', { required: 'Activity Restrictions Is Required' })} placeholder={"Activity is Active"}  defaultValue={true}/>
         {errors.isActive && <p style={{ color: 'red' }}>{errors.isActive.message}</p>}
-        <br />
 
         <label></label>
         <input type="submit" value={"Add Activity"}  />
