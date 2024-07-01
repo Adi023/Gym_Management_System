74be9d1799a8b65ed2f7d8d2a91ad55a864937e4
Adi023 <adityapandhare050@gmail.com>
2024-07-02 00:02:41 +0530

    feat : activity participation component

--- src/components/activities/Participation.jsx
+++ src/components/activities/Participation.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import ActivityMapperServices from '../../services/ActivityMapperServices';
+import { toast } from 'react-toastify';
 
 export default function Participation() {
+  
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+  const sendData = async (d) => {
+    try {
+      const data = await ActivityMapperServices.addMapperActivity(d);
+      toast.success(`Activity Participation Successfully, ID: ${data.data.id}`);
+      reset(); // Assuming reset function is defined somewhere
+    } catch (error) {
+      console.error("Error Participation Activity:", error);
+      toast.error("Failed to Participation in Activity");
+    }
+  }
+
   return (
-    <div>
+    <div className="signup-container">
+    <h2 style={{ marginLeft: '25%', position: 'relative' }}>Schedule Activity</h2>
+      <form onSubmit={handleSubmit(sendData)}>
+      <div className="form-row">
+
+        <label htmlFor="activityId">Participation Id</label>
+        <input type="text"  {...register('id', { required: 'Participation Id Is Required' })} placeholder={"Participation Id"} />
+        {errors.id && <p style={{ color: 'red' }}>{errors.id.message}</p>}
+        <br />
       
+        <label htmlFor="activityName">User Id</label>
+        <input type="number"  {...register('user', { required: 'User Id Is Required' })} placeholder={"User Id"} />
+        {errors.user && <p style={{ color: 'red' }}>{errors.user.message}</p>}
+        <br />
+    
+        <label htmlFor="Activity ScheduleId">Schedule</label>
+        <input type="text"  {...register('scheduleId', { required: 'Activity Schedule Id Is Required' })} placeholder={"Activity Schedule Id"} />
+        {errors.scheduleId && <p style={{ color: 'red' }}>{errors.scheduleId.message}</p>}
+        <br />
+
+        <label htmlFor="Activity register Date">Register Date</label>
+        <input type="date"  {...register('registerDate', { required: 'Activity register Date Is Required' })} placeholder={"Activity register Date"} />
+        {errors.registerDate && <p style={{ color: 'red' }}>{errors.registerDate.message}</p>}
+        <br />
+
+        {/* <label>Activity isActive</label> */}
+        <input type="hidden" {...register('isAttended', { required: 'Activity Restrictions Is Required' })} placeholder={"Activity is Active"}  defaultValue={false}/>
+        {errors.isAttended && <p style={{ color: 'red' }}>{errors.isAttended.message}</p>}
+
+        <label></label>
+        <input type="submit" value={"Participate in Activity"}  />
+        <br/>
+        <label></label>
+        <input type="reset" value={"Reset"}  />
+        </div>
+      </form>
     </div>
   )
 }
