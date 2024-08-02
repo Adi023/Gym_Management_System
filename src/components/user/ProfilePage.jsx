447906441c79ebc6545bc4ad6a2df10444d7e85a
Adi023 <adityapandhare050@gmail.com>
2024-08-02 09:47:26 +0530

    refactor : profile page component added

---
+++ src/components/user/ProfilePage.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import UserServices from '../../services/UserServices'
+import { useSelector } from 'react-redux';
+import back from '../images/about.jpg'
+import profile from '../images/profile.jpg'
+import './ProfileStyle.css'
+
+export default function ProfilePage() {
+    const reduxUserId = useSelector(state => state.userId);
+    const getService = () => {
+        // var userId = 1
+        // var id = localStorage.getItem('userId');
+
+        UserServices.getUserById(reduxUserId).then(
+            (Response) => {
+                // console.log(Response.data)
+                setData(Response.data)
+            },
+            (Error) => {
+                console.log(Error);
+            }
+        );
+    };
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getService();
+    });
+
+    return (
+        <div className="padding">
+            <div className="col-md-10">
+                {/* <!-- Column --> */}
+                <div className="card">
+                    <img className="card-img-top" src={back} alt="Card image cap" />
+
+                    <div className="card-body little-profile text-center">
+                        <div className="pro-img">
+                            <img src={profile} alt="user" />
+                        </div>
+
+                        <h3 className="m-b-0">{data.name ? data.name : 'N/A'}</h3>
+                        <p>{data.active ? "Active" : 'N/A'} </p>
+                        <div className="row text-center m-t-20">
+                            <div className="col-lg-4 col-md-4 m-t-20">
+                                <h3 className="m-b-0 font-light">
+                                    <small>Gender:</small> {data.gender ? data.gender : 'N/A'}<br />
+                                    <small>Blood Group:</small> {data.bloodGroup ? data.bloodGroup : 'N/A'}<br />
+                                    <small>DOB:</small> {data.dob ? data.dob : 'N/A'}<br />
+                                </h3>
+                            </div>
+                            <div className="col-lg-4 col-md-4 m-t-20">
+                                <h3 className="m-b-0 font-light">
+                                    <small> Email:</small> {data.email ? data.email : 'N/A'}<br />
+                                    <small> Mobile:</small> {data.mobile ? data.mobile : 'N/A'}<br />
+                                    <small> Address:</small> {data.address ? data.address : 'N/A'}<br />
+                                </h3>
+                            </div>
+                            <div className="col-lg-4 col-md-4 m-t-20">
+                                <h3 className="m-b-0 font-light">
+                                    <small> City:</small> {data.cityId?.cityName ? data.cityId?.cityName : 'N/A'}<br />
+                                    <small> State:</small> {data.cityId?.stateId?.stateName ? data.cityId?.stateId?.stateName : 'N/A'}<br />
+                                    <small> Country:</small> {data.cityId?.countryId?.countryName}<br />
+                                    <small> Date of Joining: </small> {data.dateOfJoining ? data.dateOfJoining : 'N/A'}<br />
+                                    <small> Activity Status: </small> {data.active ? "Active" : 'N/A'}<br />
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
