61893d6371375585fe9ba6a2725eac88c4fd1dba
Adi023 <adityapandhare050@gmail.com>
2024-09-26 00:24:07 +0530

    refactor : date format is corrected

--- src/components/Helper/TodaysDate.js
+++ src/components/Helper/TodaysDate.js
@@ -1,9 +1,11 @@
 class TodaysDate{
      getDate() {
         const today = new Date();
-        const month = today.getMonth() + 1;
+        // const month = today.getMonth() + 1;
+        const month = String(today.getMonth() + 1).padStart(2, '0'); // Adds leading 0 if needed
         const year = today.getFullYear();
-        const date = today.getDate();
+        // const date = today.getDate();
+        const date = String(today.getDate()).padStart(2, '0'); // Adds leading 0 if needed
         return `${year}-${month}-${date}`;
       }
 }
